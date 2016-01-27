7c7f65bd4e9e4c102eecb4ae87cd30590bcf332b
Patrick Sier <pjsier@gmail.com>
2016-01-26 23:54:16 -0600

    working on seeding 311 data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'json'
+
+resource_path = "db"
+json_file = File.read(Rails.root.join(resource_path, "311_pave.json"))
+json_data = JSON.parse(json_file)
+
+json_data.each do |pave_data|
+  issue = Issue.find_or_create_by(api_id: pave_data[:service_request_id],
+                                  api_status: pave_data[:status],
+                                  service_code: pave_data[:service_code],
+                                  lonlat: 'POINT(#{pave_data[:long]} #{pave_data[:lat]})')
+end
