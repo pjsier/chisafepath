3b5b188934450fefdab71452771579e5e4905f40
pjsier <pjsier@gmail.com>
2016-06-23 19:39:41 -0500

    aws cleanup

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV["AWS_ACESS_KEY_ID"]
-    aws_secret_access_key: ENV["AWS_ACCESS_KEY_ID"]
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: ENV["S3_REGION"]
   }
   config.fog_directory  = 'chisafepath'
 end
