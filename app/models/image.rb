808040d4ddbaabdcfbe3987952da5dff926fa869
Patrick Sier <pjsier@users.noreply.github.com>
2016-07-05 09:16:52 -0500

    Fix `remove_aws_img` method reference to wrong attribute

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
   def remove_aws_img
-    if self.image_url.starts_with("https://chisafepath")
+    if self.url.starts_with("https://chisafepath")
       s3 = Aws::S3::Client.new
       s3.delete_object(bucket: 'chisafepath', key: self.image_url[37..-1])
     end
